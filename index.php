9a909cfea1502ada45eea79c57cd339077c3346d
Konstantin Pereyaslov <perk11@perk11.info>
2015-10-24 04:09:34 +0300

    Первоначальный коммит, подключаем дождь 👨

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Дождь из мужиков</title>
+	<script src="bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+	<link rel="import" href="bower_components/emoji-rain/emoji-rain.html">
+	<meta charset="utf-8">
+</head>
+<body>
+<emoji-rain active></emoji-rain>
+</body>
+</html>
